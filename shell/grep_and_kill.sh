26a375542a21caa5ff14f1ad8fcfc29c2924df6e
gosudream <gosudream@gmail.com>
2012-05-14 13:24:20 +0800

    grep_and_kill shell script

---
+++ shell/grep_and_kill.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+tofind=$1
+# read by line
+IFS="
+"
+function show_processes() {
+  result=`ps -Ao pid,cmd | grep $tofind | grep -v grep`
+  for cmd in $result; do
+    echo "    $cmd"
+  done
+}
+
+echo ">> Find process(es):"
+show_processes
+
+echo -n '>> Kill all?[y/n]: '
+read anwser
+if [ "$anwser" = "y" ];then
+  #echo "Yes"
+  #continue
+  echo -n ''
+else
+  # user choose not kill the process
+  exit 0
+fi
+
+# read by line
+IFS=
+for cmd in $result; do
+  echo 'will kill -->' $cmd
+  #echo "pid: `echo $cmd | cut -d ' ' -f1`" 
+  kill -9 `echo $cmd | cut -d ' ' -f1`
+done
+
+echo ">> Now grep result is: "
+show_processes
